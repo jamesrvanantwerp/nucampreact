9e0518ba8b0792c60b72b0102f8e2a8372894062
Ted Van Antwerp <jamesrvanantwerp@gmail.com>
2022-06-06 18:11:16 -0500

    json server

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -2,23 +2,32 @@ import { Col, Row } from 'reactstrap';
 import Partner from './Partner';
 import { selectAllPartners } from './partnersSlice';
 import { useSelector } from 'react-redux';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const PartnersList = () => {
   const partners = useSelector(selectAllPartners);
+
+  const isLoading = useSelector((state) => state.partners.isLoading);
+  const errMsg = useSelector((state) => state.partners.errMsg);
   
-  return (
+  return isLoading ? (
+    <Loading />
+) : errMsg ? (
+    <Error errMsg={errMsg} />
+) : (
     <Col className='mt-4'>
-      <Row>
-        {partners.map((partner) => {
-          return (
-            <div className='d-flex mb-5' key={partner.id}>
-              <Partner partner={partner} />
-            </div>
-          );
-        })}
-      </Row>
+        <Row>
+            {partners.map((partner) => {
+                return (
+                    <div className='d-flex mb-5' key={partner.id}>
+                        <Partner partner={partner} />
+                    </div>
+                );
+            })}
+        </Row>
     </Col>
-  );
+);
 };
 
 export default PartnersList;
